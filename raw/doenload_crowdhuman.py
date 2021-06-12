564ef83d769d9d805c028b98523940bae6f8beca
naomi fridman <naomi.fridman@gmail.com>
2021-06-12 17:25:13 +0300

    Create doenload_crowdhuman.py

---
+++ raw/doenload_crowdhuman.py
@@ -0,0 +1,6 @@
+import gdown
+
+gdown.download('https://drive.google.com/uc?export=download&id=1-1noaH0ZfCDiHYw2oM1pCE6EaNOyTbOj', 'CrowdHuman_train03.zip', quiet=False)
+gdown.download('https://drive.google.com/uc?export=download&id=1ho6apKoazy1fM4JC5PYk-nH_efsb1nli', 'CrowdHuman_val.zip', quiet=False)
+gdown.download('https://drive.google.com/u/0/uc?id=1UUTea5mYqvlUObsC1Z8CFldHJAtLtMX3', 'annotation_train.odgt', quiet=False)
+gdown.download('https://drive.google.com/uc?export=download&id=1owOE8B4tohyJsnT4M5IiCUgHl1Z_vMGu','annotation_val.odgt', quiet=False)
